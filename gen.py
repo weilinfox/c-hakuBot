47c0b7e789ea274f758c200ace3c51bb966a5395
weilinfox <weilin.king@qq.com>
2021-01-03 22:13:16 +0800

    update gen

--- gen.py
+++ gen.py
@@ -2,6 +2,7 @@ import os
 import subprocess
 import json
 import time
+import requests
 from config import masterQid, defaultJson
 
 updateCode = 110
@@ -13,6 +14,8 @@ rootPath = os.getcwd() + '/' + os.path.dirname(__file__)
 cqhttpAddr = '127.0.0.1'
 cqhttpPort = 0
 cqhttpSendPort = 0
+cqhttpSendPortStr = ''
+cqhttpPortStr = ''
 cqhttpToken = ''
 if not os.path.exists(rootPath + '/c-hakuBot/config.json'):
     if not os.path.exists(rootPath + '/c-hakuBot/hakuBot'):
@@ -60,7 +63,9 @@ try:
     configDict = json.loads(jsonData)
     cqhttpAddr = configDict['URL']
     cqhttpSendPort = configDict['SEND_PORT']
+    cqhttpSendPortStr = str(cqhttpSendPort)
     cqhttpPort = configDict['PORT']
+    cqhttpPortStr = str(cqhttpPort)
     cqhttpToken = configDict['TOKEN']
     masterQid.append(configDict['MASTER0'])
     if cqhttpPort < 1024 or cqhttpPort > 49151 or cqhttpSendPort < 1024 or cqhttpSendPort > 49151:
@@ -83,8 +88,70 @@ while True:
         break
     elif rtCode == updateCode:
         print('\nStart update process.')
+        sendMsgUrl = ''
+        sendMsgParms = {}
+        sendMsgFlag = True
+        if os.path.exists(rootPath + '/c-hakuBot/quit.txt'):
+            quitFile = open(rootPath + '/c-hakuBot/quit.txt')
+            quitMsg = quitFile.read()
+            quitFile.close()
+            quitMsgList = list(quitMsg.split())
+            try:
+                quitDict = json.loads(quitMsgList[len(quitMsgList)-1])
+                if quitDict['post_type'] == 'message':
+                    if quitDict['message_type'] == 'private':
+                        sendMsgUrl = 'http://' + cqhttpAddr + ':' + cqhttpSendPortStr + '/send_private_msg'
+                        sendMsgParms = {'user_id':quitDict['user_id'], 'message':'', 'auto_escape':False}
+                        if len(cqhttpToken): sendMsgParms.update({'access_token':cqhttpToken})
+                    elif quitDict['message_type'] == 'group':
+                        sendMsgUrl = 'http://' + cqhttpAddr + ':' + cqhttpSendPortStr + '/send_group_msg'
+                        sendMsgParms = {'group_id':quitDict['group_id'], 'message':'', 'auto_escape':False}
+                        if len(cqhttpToken): sendMsgParms.update({'access_token':cqhttpToken})
+                    else:
+                        sendMsgFlag = False
+                        print('Unsupported message. ', quitDict['message_type'])
+                else:
+                    sendMsgFlag = 0;
+                    print('Quit message is not a message event?')
+            except Exception as e:
+                sendMsgFlag = False
+                print('Gen parse quit message Failed')
+                print(e)
+        else:
+            sendMsgFlag = False
+            print('No quit message?')
+                
         gitPull = subprocess.getoutput('cd ' + rootPath + '/c-hakuBot/ && git pull')
-        makeInstall = subprocess.getoutput('cd ' + rootPath + ' && make && make install && make clean')
+        if sendMsgFlag:
+            sendMsgParms['message'] = 'Git returned message:\n' + gitPull;
+            resp = requests.get(url=sendMsgUrl, params=sendMsgParms)
+            print(resp)
+        makeInstall = subprocess.getoutput('cd ' + rootPath + ' && make')
+        subprocess.getoutput('cd ' + rootPath + ' && make install && make clean')
+        makeList = list(makeInstall.split('\n', makeInstall.count('\n')))
+        #print(makeList)
+        errorFlag = False
+        sendFlag = False
+        for s in makeList:
+            #if errorFlag:
+            #    errorFlag = False
+            #    sendFlag = True
+            #    sendMsgParms['message'] = s
+            #    resp = requests.get(url=sendMsgUrl, params=sendMsgParms)
+            #    print(resp)
+            if 'warning ' in s or 'error ' in s or 'fatal' in s or 'Warning ' in s or 'Error ' in s or 'Fatal' in s or \
+                'errors' in s or 'Errors' in s or 'wainings' in s or 'Warnings' in s or \
+                'error: ' in s or 'Error: ' in s or 'warning:' in s or 'Warning:' in s:
+                errorFlag = True
+                sendFlag = True
+                sendMsgParms['message'] = s
+                resp = requests.get(url=sendMsgUrl, params=sendMsgParms)
+                print(resp)
+
+        if not sendFlag:
+            sendMsgParms['message'] = 'No errors, no warnings.'
+            resp = requests.get(url=sendMsgUrl, params=sendMsgParms)
+            print(resp)
         print('Finished. Wake haku up now.\n')
         #print(gitPull)
         #print(makeInstall)
